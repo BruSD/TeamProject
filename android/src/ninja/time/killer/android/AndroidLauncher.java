e12eb11f600bafc57697fb713a035bd250f3e019
BruSD <brusd.com@gmail.com>
2014-06-23 02:15:01 -0700

    Initial commit

---
+++ android/src/ninja/time/killer/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package ninja.time.killer.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import ninja.time.killer.NinjaGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new NinjaGame(), config);
+	}
+}
